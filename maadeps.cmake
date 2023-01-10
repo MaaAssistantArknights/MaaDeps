85aea8846a46b0df92e63a47cf1c63146398c33d
dantmnf <dantmnf@users.noreply.github.com>
2023-01-10 23:52:17 +0800

    update build system integration

--- maadeps.cmake
+++ maadeps.cmake
@@ -1,14 +1,14 @@
-if(NOT DEFINED VCPKG_TARGET_TRIPLET)
+if(NOT DEFINED MAADEPS_TRIPLET)
   find_package(Python3 COMPONENTS Interpreter REQUIRED)
   execute_process(COMMAND "${Python3_EXECUTABLE}" "${CMAKE_CURRENT_LIST_DIR}/host_triplet.py" OUTPUT_VARIABLE VCPKG_HOST_TRIPLET OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(VCPKG_TARGET_TRIPLET "maa-${VCPKG_HOST_TRIPLET}")
+  set(MAADEPS_TRIPLET "maa-${VCPKG_HOST_TRIPLET}")
 endif()
-if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/vcpkg/installed/${VCPKG_TARGET_TRIPLET}")
+if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/vcpkg/installed/${MAADEPS_TRIPLET}")
   message(FATAL_ERROR
     " "
-    " Dependencies not found for ${VCPKG_TARGET_TRIPLET}\n"
+    " Dependencies not found for ${MAADEPS_TRIPLET}\n"
     " run MaaDeps/download.py to download prebuilt binaries\n"
     " or MaaDeps/build.py to build from source\n"
   )
 endif()
-list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/vcpkg/installed/${VCPKG_TARGET_TRIPLET}")
+list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/vcpkg/installed/${MAADEPS_TRIPLET}")
