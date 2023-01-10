d04dd1c66f3b4701aa74b5ecb61301cf20983183
dantmnf <dantmnf@users.noreply.github.com>
2023-01-10 23:53:46 +0800

    x64-linux

--- triplets/maa-x64-linux.cmake
+++ triplets/maa-x64-linux.cmake
@@ -1,5 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake.inc)
