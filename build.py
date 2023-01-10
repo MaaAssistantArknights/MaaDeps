dcf0feb4334ed65fe7956dd32924cbfcc3926751
dantmnf <dantmnf@users.noreply.github.com>
2023-01-11 00:23:59 +0800

    update paddle2onnx

--- build.py
+++ build.py
@@ -150,7 +150,7 @@ def main(argv: list[str]):
     invoke_cmake_install(os.path.join(ort.src, "cmake"), ort.bin, *common_cmake_args, *ort_cmake_args)
 
     paddle2onnx = src_bin_dirs("paddle2onnx")
-    clone_git_repo("https://github.com/MaaAssistantArknights/Paddle2ONNX", "8718eb81a6a21f13fbde7b79f3b8745c9aee0491", paddle2onnx.src, skip_update=config.skip_src_update)
+    clone_git_repo("https://github.com/MaaAssistantArknights/Paddle2ONNX", "8a7f40973820f9b7aba4d5deeb2ba14f93f911af", paddle2onnx.src, skip_update=config.skip_src_update)
 
     fastdeploy = src_bin_dirs("fastdeploy")
     clone_git_repo("https://github.com/MaaAssistantArknights/FastDeploy", "9cb37186b72c032e59ccaf61d55c7ba7e8bcadb0", fastdeploy.src, skip_update=config.skip_src_update)
