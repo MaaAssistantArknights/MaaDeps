c8d72488ce6e7ed520bae43aca1936dbc9e19c09
dantmnf <dantmnf@users.noreply.github.com>
2023-04-23 23:36:23 +0800

    update ports

--- vcpkg-overlay/ports/maa-onnxruntime/portfile.cmake
+++ vcpkg-overlay/ports/maa-onnxruntime/portfile.cmake
@@ -7,53 +7,11 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/onnxruntime
-    REF v1.12.1
-    SHA512 fc2e8be54fbeb32744c8882e61aa514be621eb621a073d05a85c6e2deac8c9bf1103e746711f5c33a4fa55a257807ba0159d9f23684f4926ff38b40591575d91
+    REF v1.14.1
+    SHA512 d8f7ea161e850a738b9a22187662218871f88ad711282c58631196a74f4a4567184047bab0001b973f841a3b63c7dc7e350f92306cc5fa9a7adc4db2ce09766f
     PATCHES
         0000-system-lib-fix.patch
 )
-# vcpkg_find_acquire_program(GIT)
-
-# set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/maa-onnxruntime-1.12.1")
-
-# make_directory("${SOURCE_PATH}")
-
-# set(onnxruntime_GIT_REF 70481649e3c2dba0f0e1728d15a00e440084a217)  # v1.12.1
-
-# # download
-# if(NOT EXISTS "${SOURCE_PATH}/.git")
-#     vcpkg_execute_required_process(
-#         ALLOW_IN_DOWNLOAD_MODE
-#         COMMAND "${GIT}" init -b unused_branch
-#         WORKING_DIRECTORY "${SOURCE_PATH}"
-#         LOGNAME LOGNAME "maa-onnxruntime-fetch-${TARGET_TRIPLET}"
-#     )
-# endif()
-# vcpkg_execute_required_process(
-#     ALLOW_IN_DOWNLOAD_MODE
-#     COMMAND "${GIT}" fetch --depth 1 https://github.com/microsoft/onnxruntime "${onnxruntime_GIT_REF}"
-#     WORKING_DIRECTORY "${SOURCE_PATH}"
-#     LOGNAME LOGNAME "maa-onnxruntime-fetch-${TARGET_TRIPLET}"
-# )
-# vcpkg_execute_required_process(
-#     ALLOW_IN_DOWNLOAD_MODE
-#     COMMAND "${GIT}" reset --hard FETCH_HEAD
-#     WORKING_DIRECTORY "${SOURCE_PATH}"
-#     LOGNAME LOGNAME "maa-onnxruntime-fetch-${TARGET_TRIPLET}"
-# )
-# vcpkg_apply_patches(
-#     SOURCE_PATH "${SOURCE_PATH}"
-#     PATCHES 0000-system-lib-fix.patch
-# )
-
-# foreach(submodule onnx)
-#     vcpkg_execute_required_process(
-#         ALLOW_IN_DOWNLOAD_MODE
-#         COMMAND "${GIT}" submodule update --init --recommend-shallow --depth 1 "cmake/external/${submodule}" 
-#         WORKING_DIRECTORY "${SOURCE_PATH}"
-#         LOGNAME LOGNAME "maa-onnxruntime-fetch-${TARGET_TRIPLET}"
-#     )
-# endforeach()
 
 vcpkg_find_acquire_program(PYTHON3)
 set(FLATC_EXECUTABLE "${CURRENT_HOST_INSTALLED_DIR}/tools/flatbuffers/flatc${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
@@ -66,11 +24,11 @@ vcpkg_execute_build_process(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
     OPTIONS
+    "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
     "-Donnxruntime_BUILD_SHARED_LIB=ON"
     "-Donnxruntime_BUILD_UNIT_TESTS=OFF"
     "-Donnxruntime_ENABLE_LTO=ON"
     "-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"
-    "-Donnxruntime_PREFER_SYSTEM_LIB=ON"
     "-DProtobuf_USE_STATIC_LIBS=ON"
     "-Donnxruntime_USE_PREINSTALLED_EIGEN=ON"
     "-DFLATBUFFERS_BUILD_FLATC=OFF"
--- vcpkg-overlay/ports/onnx/portfile.cmake
+++ vcpkg-overlay/ports/onnx/portfile.cmake
@@ -3,10 +3,11 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO onnx/onnx
-    REF v1.12.0
-    SHA512 ab0c4f92358e904c2f28d98b35eab2d6eac22dd0a270e4f45ee590aa1ad22d09e92b32225efd7e98edb1531743f150526d26e0cbdc537757784bef2bc93efa8e
+    REF "v${VERSION}"
+    SHA512 325859f591dece43a083a0945aefe3427bfdb68a98ef5922343bf7ed959528947e7664d6c8e3e3d35c390d6c20ef22d07c672e5311f80c72c199931be6c256c3
     PATCHES
-        0000-fix-dependency.patch
+        fix-cmakelists.patch
+        fix-dependency-protobuf.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
@@ -20,12 +21,24 @@ else()
     set(USE_PROTOBUF_SHARED OFF)
 endif()
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        pybind11 BUILD_ONNX_PYTHON
+)
+
 # Like protoc, python is required for codegen.
 vcpkg_find_acquire_program(PYTHON3)
 
 # PATH for .bat scripts so it can find 'python'
 get_filename_component(PYTHON_DIR "${PYTHON3}" PATH)
 vcpkg_add_to_path(PREPEND "${PYTHON_DIR}")
+
+if("pybind11" IN_LIST FEATURES)
+    # When BUILD_ONNX_PYTHON, we need Development component. Give a hint for FindPython3
+    list(APPEND FEATURE_OPTIONS
+        "-DPython3_ROOT_DIR=${CURRENT_INSTALLED_DIR}"
+    )
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -44,10 +57,14 @@ vcpkg_cmake_configure(
         ONNX_USE_MSVC_STATIC_RUNTIME
 )
 
+if("pybind11" IN_LIST FEATURES)
+    # This target is not in install/export
+    vcpkg_cmake_build(TARGET onnx_cpp2py_export)
+endif()
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ONNX)
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
@@ -76,4 +93,6 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_ml"
     "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_data"
     "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_operators_ml"
+    "${CURRENT_PACKAGES_DIR}/include/onnx/reference/ops"
+    "${CURRENT_PACKAGES_DIR}/include/onnx/reference"
 )
