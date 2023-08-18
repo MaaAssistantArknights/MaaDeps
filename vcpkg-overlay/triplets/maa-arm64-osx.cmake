13f20c612f47e15d329f15ccd07723025d623806
Hao Guan <10684225+hguandl@users.noreply.github.com>
2023-08-18 18:19:22 +1000

    fix: macOS debug symbols

--- vcpkg-overlay/triplets/maa-arm64-osx.cmake
+++ vcpkg-overlay/triplets/maa-arm64-osx.cmake
@@ -6,4 +6,7 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_OSX_DEPLOYMENT_TARGET 11.0)
 
+string(APPEND VCPKG_C_FLAGS "-g -DNDEBUG")
+string(APPEND VCPKG_CXX_FLAGS "-g -DNDEBUG")
+
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake.inc)
