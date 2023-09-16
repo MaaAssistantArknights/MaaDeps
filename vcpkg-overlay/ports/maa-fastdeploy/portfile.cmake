c28f3505ed8435db14bf8c7a8cecef94c41bf60d
MistEO <mistereo@hotmail.com>
2023-09-17 04:53:52 +0800

    test

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -1,8 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO MaaAssistantArknights/FastDeploy
-    REF 5d576a9dceae33854fc9569f97251a14d7dacdbb
-    SHA512 fcd255a7e130805e8346ecc977017d2f80082c46a442c20f39a9c75a2be5e3d3a50d10afe4328e6ce150b44ed1e0b6f1a8f9d0d5d21cde6c57981921709087bd
+    REF 2279e19c150c4fc371a9be291f16f3d52633703d
+    SHA512 0
+    HEAD_REF maadeps
     PATCHES
         000-fix-rpath.patch
 )
