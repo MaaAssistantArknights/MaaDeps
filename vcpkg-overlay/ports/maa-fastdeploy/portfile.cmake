6b9402ac879a1c716f84b798a877c671fa0a07d5
MistEO <mistereo@hotmail.com>
2023-09-16 13:56:44 +0800

    chore: update fastdeploy for remove all cout

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO MaaAssistantArknights/FastDeploy
-    REF 1e4f600e5e5ab23528f77b98a8c5167b46ddfce2
-    SHA512 65c3bf3203fe4fd39172ee98c174b89bcf72b45e904b1ff5544da4aba45f816a4072339a165ba62e96f6055eac649e1941b0d938aa7e2ea19578d33685013c38
+    REF 5d576a9dceae33854fc9569f97251a14d7dacdbb
+    SHA512 0
     PATCHES
         000-fix-rpath.patch
 )
