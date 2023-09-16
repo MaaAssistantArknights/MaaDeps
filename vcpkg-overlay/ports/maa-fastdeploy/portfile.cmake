1a4c849c1f93d494b1c4d43f342846bd0bf3e8d0
MistEO <mistereo@hotmail.com>
2023-09-16 17:37:06 +0800

    fix: fastdeploy hash

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO MaaAssistantArknights/FastDeploy
     REF 5d576a9dceae33854fc9569f97251a14d7dacdbb
-    SHA512 0
+    SHA512 fcd255a7e130805e8346ecc977017d2f80082c46a442c20f39a9c75a2be5e3d3a50d10afe4328e6ce150b44ed1e0b6f1a8f9d0d5d21cde6c57981921709087bd
     PATCHES
         000-fix-rpath.patch
 )
