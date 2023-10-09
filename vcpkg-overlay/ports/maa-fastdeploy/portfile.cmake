fc3cb58924fbd5f323b1bd93a12bdbf7566ff12a
nekosu <liao.hy@outlook.com>
2023-10-10 03:07:22 +0800

    fix: fastdeploy export debug config

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(
     SHA512 9a328a8113707d7883e128d74217f21bd5e7241e59773709b4e8894e4491cb47b363080231399d43ced4b118e3023aaf856bef2c5a573de5091d7740ec885213
     PATCHES
         000-fix-rpath.patch
+        001-fix-export.patch
 )
 
 vcpkg_cmake_configure(
@@ -12,6 +13,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME fastdeploy_ppocr CONFIG_PATH share/fastdeploy_ppocr)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
