34c3e33d62540b4e3809f824c63024168101c629
MistEO <mistereo@hotmail.com>
2023-12-21 10:11:42 +0800

    target_compile_options

--- vcpkg-overlay/triplets/maa-arm64-android.cmake
+++ vcpkg-overlay/triplets/maa-arm64-android.cmake
@@ -8,8 +8,4 @@ set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DANDROID_USE
                                     -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-s")
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake.inc)
-
-if(PORT MATCHES "onnxruntime")
-    message("add -Wno-error=shorten-64-to-32 for ${PORT}")
-    add_compile_options(" -Wno-error=shorten-64-to-32")
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/maa-android-library-override.cmake.inc)
--- vcpkg-overlay/triplets/maa-x64-android.cmake
+++ vcpkg-overlay/triplets/maa-x64-android.cmake
@@ -7,8 +7,4 @@ set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DANDROID_USE_LEGACY_TOOLCHAIN_FILE=OFF)
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake.inc)
-
-if(PORT MATCHES "onnxruntime")
-    message("add -Wno-error=shorten-64-to-32 for ${PORT}")
-    add_compile_options(" -Wno-error=shorten-64-to-32")
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/maa-android-library-override.cmake.inc)
