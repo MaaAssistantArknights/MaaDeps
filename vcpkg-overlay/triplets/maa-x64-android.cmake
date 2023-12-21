d48ba7224dd9546d6ff612aeefe98117257f63b8
MistEO <mistereo@hotmail.com>
2023-12-21 09:58:25 +0800

    PORT MATCHES "onnxruntime"

--- vcpkg-overlay/triplets/maa-x64-android.cmake
+++ vcpkg-overlay/triplets/maa-x64-android.cmake
@@ -8,7 +8,7 @@ set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DANDROID_USE
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake.inc)
 
-if(PORT MATCHES "onnxruntime|maa-")
+if(PORT MATCHES "onnxruntime")
     message("add -Wno-error=shorten-64-to-32 for ${PORT}")
     add_compile_options(" -Wno-error=shorten-64-to-32")
 endif()
