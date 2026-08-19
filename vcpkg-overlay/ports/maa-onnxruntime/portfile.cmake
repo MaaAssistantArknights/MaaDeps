93f4e0125e42d2f8f90cea3d5d8b63f691501d6c
dantmnf <2252500+dantmnf@users.noreply.github.com>
2026-08-19 15:47:24 +0800

    feat: webgpu in onnxruntime

--- vcpkg-overlay/ports/maa-onnxruntime/portfile.cmake
+++ vcpkg-overlay/ports/maa-onnxruntime/portfile.cmake
@@ -1,31 +1,23 @@
-set(ORT_COMMIT "26250ae74d2c9a3c6860625ba4a147ddfb936907")
-set(ORT_BRANCH "v${VERSION}")
+# https://github.com/microsoft/onnxruntime/blob/v1.22.1/tools/python/util/vcpkg_helpers.py
+message(WARNING "The port requires 'onnx' port build with CMake option ONNX_DISABLE_STATIC_REGISTRATION=ON")
+if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
+    if("framework" IN_LIST FEATURES)
+        # The Objective-C API requires onnxruntime_BUILD_SHARED_LIB
+        vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+    endif()
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/onnxruntime
-    REF ${ORT_BRANCH}
-    SHA512 3bf25e431d175c61953d28b1bf8f6871376684263992451a5b2a66e670768fc66e7027f141c6e3f4d1eddeebeda51f31ea0adf4749e50d99ee89d0a26bec77ce
+    REF "v${VERSION}"
+    SHA512 9ea05f5279fc93fd243a967f3664aaabe9216f6e3ce9358ead662653984476e9cb0cb950947f31f8602541224c946a3ceac91d7c1990ffe2b8314d61361c300f
     PATCHES
-        0000-system-lib-fix.patch
-        0001-coremltools.patch
-        revert-pr-21492.patch
-        fix-pr-21348.patch # cmake, source changes of PR 21348
-        fix-cmake.patch
+        fix-cmake.patch # .framework install, external library workarounds(abseil-cpp, eigen3)
         fix-cmake-cuda.patch
-        fix-cmake-tensorrt.patch # TENSORRT_ROOT is not defined, use CUDAToolkit to search TensorRT
-        fix-llvm-rc-unicode.patch
-        fix-clang-cl-simd-compile.patch
-        fix-linux-clang.patch
-        include-chrono.patch
-        fix-pr-25815.patch
-        fix-pr-23260.patch
-        fix-pr-23872.patch
+        # fix-cmake-mlas.patch  # dropped due to no usage
+        fix-webgpu.patch
 )
-# copied from PR 21348 to reduce patch size
-# caution: https://github.com/microsoft/onnxruntime/blob/45737400a2f3015c11f005ed7603611eaed306a6/cmake/deps.txt#L20-L25
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/onnxruntime_external_deps.cmake" DESTINATION "${SOURCE_PATH}/cmake/external")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/project-config-template.cmake" DESTINATION "${SOURCE_PATH}/cmake")
 
 find_program(PROTOC NAMES protoc PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf" REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
 message(STATUS "Using protoc: ${PROTOC}")
@@ -39,7 +31,12 @@ message(STATUS "Using python3: ${PYTHON3}")
 
 vcpkg_execute_required_process(
     COMMAND "${PYTHON3}" onnxruntime/core/flatbuffers/schema/compile_schema.py --flatc "${FLATC}"
-    LOGNAME compile_schema
+    LOGNAME compile_schema_core
+    WORKING_DIRECTORY "${SOURCE_PATH}"
+)
+vcpkg_execute_required_process(
+    COMMAND "${PYTHON3}" onnxruntime/lora/adapter_format/compile_schema.py --flatc "${FLATC}"
+    LOGNAME compile_schema_lora
     WORKING_DIRECTORY "${SOURCE_PATH}"
 )
 
@@ -60,39 +57,43 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         mimalloc  onnxruntime_USE_MIMALLOC
         valgrind  onnxruntime_USE_VALGRIND
         xnnpack   onnxruntime_USE_XNNPACK
+        kleidiai  onnxruntime_USE_KLEIDIAI
         nnapi     onnxruntime_USE_NNAPI_BUILTIN
         azure     onnxruntime_USE_AZURE
-        llvm      onnxruntime_USE_LLVM
-        rocm      onnxruntime_USE_ROCM
         test      onnxruntime_BUILD_UNIT_TESTS
         test      onnxruntime_BUILD_BENCHMARKS
         test      onnxruntime_RUN_ONNX_TESTS
         framework onnxruntime_BUILD_APPLE_FRAMEWORK
         framework onnxruntime_BUILD_OBJC
         nccl      onnxruntime_USE_NCCL
-        mpi       onnxruntime_USE_MPI
+        webgpu    onnxruntime_USE_WEBGPU
+        webgpu    onnxruntime_USE_EXTERNAL_DAWN
+        webgpu    onnxruntime_BUILD_DAWN_SHARED_LIBRARY
     INVERTED_FEATURES
         cuda      onnxruntime_USE_MEMORY_EFFICIENT_ATTENTION
 )
 
-if("tensorrt" IN_LIST FEATURES)
-    if(DEFINED TENSORRT_ROOT) # if the variable in the triplet, use it
-        message(STATUS "Using TensorRT: ${TENSORRT_ROOT}")
-        list(APPEND FEATURE_OPTIONS "-Donnxruntime_TENSORRT_HOME:PATH=${TENSORRT_ROOT}")
-    endif()
+if("cuda" IN_LIST FEATURES)
+    vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root)
+    list(APPEND FEATURE_OPTIONS
+        "-DCMAKE_CUDA_COMPILER=${NVCC}"
+        "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
+        # "-DCMAKE_CUDA_ARCHITECTURES=native"
+        # too much warnings about attribute
+        "-DCMAKE_CUDA_FLAGS=-Xcudafe --diag_suppress=2803 -Wno-deprecated-gpu-targets"
+    )
 endif()
 
-if("coreml" IN_LIST FEATURES)
-    if(NOT DEFINED COREMLTOOLS_ROOT)
-        vcpkg_from_github(
-            OUT_SOURCE_PATH COREMLTOOLS_ROOT
-            REPO apple/coremltools
-            REF 8.1
-            SHA512 666f93eb29abdebba93e686486f4cd92f8e9e1a07e835b8e6cb652e6d0ba3ab2bd62d8743e7b121375abcb5f26939c0ac57f0262c28ac3b3aa8c17b75b93fbfd
-        )
+if("tensorrt" IN_LIST FEATURES)
+    if(DEFINED ENV{TENSORRT_HOME})
+        set(TENSORRT_HOME "$ENV{TENSORRT_HOME}")
     endif()
-    message(STATUS "Using CoreML: ${COREMLTOOLS_ROOT}")
-    list(APPEND FEATURE_OPTIONS "-Donnxruntime_COREMLTOOLS_HOME:PATH=${COREMLTOOLS_ROOT}")
+    if(DEFINED TENSORRT_HOME)
+        message(STATUS "Using TensorRT: ${TENSORRT_HOME}")
+        list(APPEND FEATURE_OPTIONS "-Donnxruntime_TENSORRT_HOME:PATH=${TENSORRT_HOME}")
+    else()
+        message(WARNING "Define TENSORRT_HOME for onnxruntime_TENSORRT_HOME")
+    endif()
 endif()
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
@@ -101,90 +102,66 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
     OPTIONS
+        "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
         ${FEATURE_OPTIONS}
         "-DPython_EXECUTABLE:FILEPATH=${PYTHON3}"
         "-DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
         "-DONNX_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
-        -DBUILD_PKGCONFIG_FILES=${BUILD_SHARED}
-        -Donnxruntime_USE_VCPKG=ON
+        -DBUILD_PKGCONFIG_FILES=ON
         -Donnxruntime_BUILD_SHARED_LIB=${BUILD_SHARED}
-        -Donnxruntime_BUILD_WEBASSEMBLY=OFF
         -Donnxruntime_CROSS_COMPILING=${VCPKG_CROSSCOMPILING}
         -Donnxruntime_USE_EXTENSIONS=OFF
         -Donnxruntime_USE_NNAPI_BUILTIN=${VCPKG_TARGET_IS_ANDROID}
+        -Donnxruntime_USE_VCPKG=ON
         -Donnxruntime_ENABLE_CPUINFO=ON
         -Donnxruntime_ENABLE_MICROSOFT_INTERNAL=OFF
-        -Donnxruntime_ENABLE_BITCODE=${VCPKG_TARGET_IS_IOS}
+        -Donnxruntime_ENABLE_BITCODE=OFF
         -Donnxruntime_ENABLE_PYTHON=OFF
         -Donnxruntime_ENABLE_EXTERNAL_CUSTOM_OP_SCHEMAS=OFF
+        -Donnxruntime_ENABLE_MEMORY_PROFILE=OFF
         -Donnxruntime_ENABLE_LAZY_TENSOR=OFF
         -Donnxruntime_DISABLE_RTTI=OFF
         -Donnxruntime_DISABLE_ABSEIL=OFF
-        -DORT_GIT_COMMIT=${ORT_COMMIT}
-        -DORT_GIT_BRANCH=${ORT_BRANCH}
+        # some other customizations ...
         --compile-no-warning-as-error
     OPTIONS_DEBUG
         -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
-        -Donnxruntime_ENABLE_MEMORY_PROFILE=OFF
-        -Donnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=OFF
+        -Donnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=1
     MAYBE_UNUSED_VARIABLES
-        onnxruntime_BUILD_WEBASSEMBLY
-        onnxruntime_TENSORRT_PLACEHOLDER_BUILDER
-        onnxruntime_USE_CUSTOM_DIRECTML
         Python_EXECUTABLE
-        ORT_GIT_COMMIT
-        ORT_GIT_BRANCH
+        onnxruntime_TENSORRT_PLACEHOLDER_BUILDER
+        onnxruntime_NVCC_THREADS
+        CMAKE_CUDA_FLAGS
+        onnxruntime_USE_CUSTOM_DIRECTML
 )
-
 if("cuda" IN_LIST FEATURES)
     vcpkg_cmake_build(TARGET onnxruntime_providers_cuda LOGFILE_BASE build-cuda)
 endif()
 if("tensorrt" IN_LIST FEATURES)
     vcpkg_cmake_build(TARGET onnxruntime_providers_tensorrt LOGFILE_BASE build-tensorrt)
 endif()
-if("coreml" IN_LIST FEATURES)
-    vcpkg_cmake_build(TARGET onnxruntime_providers_coreml LOGFILE_BASE build-coreml)
-endif()
-if("directml" IN_LIST FEATURES)
-    # vcpkg_cmake_build(TARGET onnxruntime_providers_dml LOGFILE_BASE build-dml)
-endif()
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/onnxruntime)
-if(BUILD_SHARED)
-    vcpkg_fixup_pkgconfig() # pkg_check_modules(libonnxruntime)
-endif()
+vcpkg_fixup_pkgconfig() # pkg_check_modules(libonnxruntime)
 
-# cmake function which relocates the onnxruntime_providers_* library before vcpkg_copy_pdbs()
-function(relocate_ort_providers PROVIDER_NAME)
+# relocates the onnxruntime_providers_* binaries before vcpkg_copy_pdbs()
+function(reolocate_ort_providers)
     if(VCPKG_TARGET_IS_WINDOWS AND (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic"))
         # the target is expected to be used without the .lib files
-        file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/${PROVIDER_NAME}.dll"
-                    "${CURRENT_PACKAGES_DIR}/debug/bin/${PROVIDER_NAME}.dll")
-        file(RENAME "${CURRENT_PACKAGES_DIR}/lib/${PROVIDER_NAME}.dll"
-                    "${CURRENT_PACKAGES_DIR}/bin/${PROVIDER_NAME}.dll")
+        file(GLOB PROVIDE_BINS_DBG  "${CURRENT_PACKAGES_DIR}/debug/lib/onnxruntime_providers_*.dll")
+        file(COPY ${PROVIDE_BINS_DBG} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+        file(GLOB PROVIDE_BINS_REL "${CURRENT_PACKAGES_DIR}/lib/onnxruntime_providers_*.dll")
+        file(COPY ${PROVIDE_BINS_REL} DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+        file(REMOVE ${PROVIDE_BINS_DBG} ${PROVIDE_BINS_REL})
     endif()
 endfunction()
 
-if("cuda" IN_LIST FEATURES)
-    relocate_ort_providers(onnxruntime_providers_cuda)
-endif()
-if("tensorrt" IN_LIST FEATURES)
-    relocate_ort_providers(onnxruntime_providers_tensorrt)
-endif()
-if("coreml" IN_LIST FEATURES)
-    relocate_ort_providers(onnxruntime_providers_coreml)
-endif()
-if("directml" IN_LIST FEATURES)
-    # relocate_ort_providers(onnxruntime_providers_dml)
-endif()
+reolocate_ort_providers()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/debug/bin"
-        "${CURRENT_PACKAGES_DIR}/bin"
-    )
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
--- vcpkg-overlay/ports/onnx/portfile.cmake
+++ vcpkg-overlay/ports/onnx/portfile.cmake
@@ -3,14 +3,18 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO onnx/onnx
-    REF "v${VERSION}"
-    SHA512 7a9a8493b9c007429629484156487395044506f34e72253640e626351cb623b390750b36af78a290786131e3dcac35f4eb269e8693b594b7ce7cb105bcf9318d
+    REF "v1.22.0"
+    SHA512 13fafff073a8e0bcf67fd06195da979c3c6273b3f5fdd25f5e5c39ad6af20eefe107f13dc73d52b6021e986d87401e46121e00bba8fbd09f098334e34f78462f
     PATCHES
         fix-cmakelists.patch
         fix-dependency-protobuf.patch
+        binskim.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
+
+# ONNX_CUSTOM_PROTOC_EXECUTABLE
+find_program(PROTOC NAMES protoc PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf" REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
 
 # ONNX_USE_PROTOBUF_SHARED_LIBS: find the library and check its file extension
 find_library(PROTOBUF_LIBPATH NAMES protobuf PATHS "${CURRENT_INSTALLED_DIR}/bin" "${CURRENT_INSTALLED_DIR}/lib" REQUIRED)
@@ -21,29 +25,17 @@ else()
     set(USE_PROTOBUF_SHARED OFF)
 endif()
 
-# Like protoc, python is required for codegen.
 vcpkg_find_acquire_program(PYTHON3)
-
-# PATH for .bat scripts so it can find 'python'
-get_filename_component(PYTHON_DIR "${PYTHON3}" PATH)
-vcpkg_add_to_path(PREPEND "${PYTHON_DIR}")
-
-if("pybind11" IN_LIST FEATURES)
-    # When BUILD_ONNX_PYTHON, we need Development component. Give a hint for FindPython3
-    list(APPEND FEATURE_OPTIONS
-        "-DPython3_ROOT_DIR=${CURRENT_INSTALLED_DIR}"
-    )
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DPython3_EXECUTABLE=${PYTHON3}
+        "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
+        "-DONNX_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
         -DONNX_ML=ON
-        -DONNX_GEN_PB_TYPE_STUBS=ON
         -DONNX_USE_PROTOBUF_SHARED_LIBS=${USE_PROTOBUF_SHARED}
-        -DONNX_USE_LITE_PROTO=OFF
         -DONNX_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
         -DONNX_BUILD_TESTS=OFF
         -DONNX_BUILD_BENCHMARKS=OFF
@@ -51,16 +43,10 @@ vcpkg_cmake_configure(
     MAYBE_UNUSED_VARIABLES
         ONNX_USE_MSVC_STATIC_RUNTIME
 )
-
-if("pybind11" IN_LIST FEATURES)
-    # This target is not in install/export
-    vcpkg_cmake_build(TARGET onnx_cpp2py_export)
-endif()
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ONNX)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ONNX PACKAGE_NAME ONNX)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/ONNXConfig.cmake" "set(Protobuf_INCLUDE_DIR" "#set(Protobuf_INCLUDE_DIR")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
