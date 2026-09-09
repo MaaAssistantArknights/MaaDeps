5e885741756894d4bc405ba99825e647c8efcc46
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2026-09-09 16:00:39 +0800

    build: opencv 裁剪不需要的模块

--- vcpkg-overlay/triplets/maa-linux-library-override.cmake
+++ vcpkg-overlay/triplets/maa-linux-library-override.cmake
@@ -1,19 +1,26 @@
 set(VCPKG_LIBRARY_LINKAGE static)
 # set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DCMAKE_SHARED_LIBRARY_SUFFIX_CXX=_maa.so)
 
-if(PORT STREQUAL "opencv4")
+if(PORT MATCHES "^opencv")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
-  set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DWITH_V4L=OFF)
+  list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
+    -DWITH_V4L=OFF
+    -DBUILD_opencv_ml=OFF
+    -DBUILD_opencv_stitching=OFF
+    -DBUILD_opencv_java_bindings_generator=OFF
+    -DBUILD_opencv_js_bindings_generator=OFF
+    -DBUILD_opencv_objc_bindings_generator=OFF
+    -DBUILD_opencv_python_bindings_generator=OFF
+    -DBUILD_opencv_python_tests=OFF
+    -DBUILD_opencv_hdf=OFF
+    -DBUILD_opencv_quality=OFF
+  )
 endif()
 
 if(PORT MATCHES "onnxruntime|maa-")
   message("setting dynamic linkage for ${PORT}")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
-
-if (PORT STREQUAL "opencv")
-    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS -DBUILD_opencv_hdf=OFF -DBUILD_opencv_quality=OFF)
-endif ()
 
 if (PORT STREQUAL "wayland")
   set(X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES ON)
--- vcpkg-overlay/triplets/maa-windows-library-override.cmake
+++ vcpkg-overlay/triplets/maa-windows-library-override.cmake
@@ -1,20 +1,27 @@
 set(VCPKG_LIBRARY_LINKAGE static)
 
-if(PORT STREQUAL "opencv4")
+if(PORT MATCHES "^opencv")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
+  list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
+    -DBUILD_opencv_ml=OFF
+    -DBUILD_opencv_stitching=OFF
+    -DBUILD_opencv_java_bindings_generator=OFF
+    -DBUILD_opencv_js_bindings_generator=OFF
+    -DBUILD_opencv_objc_bindings_generator=OFF
+    -DBUILD_opencv_python_bindings_generator=OFF
+    -DBUILD_opencv_python_tests=OFF
+    -DBUILD_opencv_hdf=OFF
+    -DBUILD_opencv_quality=OFF
+  )
 endif()
 
 if(PORT STREQUAL "maa-onnxruntime")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-  endif()
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
 
 if(PORT STREQUAL "maa-fastdeploy")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS} -DCMAKE_SHARED_LIBRARY_SUFFIX_CXX=_maa.dll)
-
-if (PORT STREQUAL "opencv")
-    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS -DBUILD_opencv_hdf=OFF -DBUILD_opencv_quality=OFF)
-endif ()
 
